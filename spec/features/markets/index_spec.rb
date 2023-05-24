0bd500b898203783fc2e346ec52baeaa9ae111ff
Brian Guthrie <takingcareofthedeadfish@gmail.com>
2023-05-24 16:55:25 -0600

    Test: add test for market index page

--- spec/features/markets/index_spec.rb
+++ spec/features/markets/index_spec.rb
@@ -3,9 +3,22 @@ require 'rails_helper'
 RSpec.describe 'Market index page', type: :feature do
   describe 'As a visitor' do
     it 'I see all marked listed with their name, city, and state' do
+      markets = MarketFacade.get_markets
 
+      visit '/markets'
+
+      expect(page).to have_content("Markets")
+      expect(page).to have_content("Name")
+      expect(page).to have_content("City")
+      expect(page).to have_content("State")
+
+      markets.each do |market|
+        expect(page).to have_content(market.name)
+        expect(page).to have_content(market.city)
+        expect(page).to have_content(market.state)
+      end
     end
-    
+
     it 'has a button to see more info on that market that takes me to /markets/:id'
   end
 end
