d55c16a0b7f14bc6d042e73fad19b17c865488e3
Brian Guthrie <takingcareofthedeadfish@gmail.com>
2023-05-25 10:24:28 -0600

    Test: for vendor show page

---
+++ spec/features/vendors/show_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe 'Vendor Show Page' do
+  describe 'As a visitor' do
+    it 'I see the vendors name, contact information, whether they accept credit, and a description of what they sell' do
+      vendor = MarketFacade.new.single_vendor(55823)
+
+      expect(page).to have_content("Contact Info:")
+      expect(page).to have_content("name: #{vendor.name}")
+      expect(page).to have_content("phone #{vendor.contact_phone}")
+      expect(page).to have_content("Credit Accepted?: #{vendor.credit_accepted}")
+      expect(page).to have_content("Description: #{vendor.description}")
+    end
+  end
+end
+
+# As a visitor
+# When I visit a vendor's show page 'vendors/:id'
+# I see that vendor's name, contact information, whether they accept credit, and a description of what they sell
